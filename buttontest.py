af25e1df3044b3d2e617a71da7df56da15b68c2e
fuseberry <roman@visionberry.de>
2024-06-13 11:43:41 +0100

    pika

---
+++ buttontest.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+from waveshare_epd import epd2in13_V4
+from gpiozero import Button
+from signal import pause
+from gpiozero import TonalBuzzer
+from gpiozero.tones import Tone
+from time import sleep
+
+# gpio setup
+tb = TonalBuzzer(5)
+btn_links = Button(19)
+btn_rechts = Button(20)
+btn_oben = Button(16)
+btn_unten = Button(26)
+btn_a = Button(6)
+btn_b = Button(12)
+
+
+def sb1():
+    print("GPIO 16 Button 'oben' was pressed!")
+    global tb
+    f = 392.0
+    tb.play(Tone.from_frequency(f))
+    sleep(0.2)
+    tb.stop()
+        
+
+def sb2():
+    print("Goodbye! 19 links")
+    global tb
+    f = 440.0
+    tb.play(Tone.from_frequency(f))
+    sleep(0.2)
+    tb.stop()
+
+def sb3():
+    print("Goodbye! 20 rechts" )
+    global tb
+    f = 493.88
+    tb.play(Tone.from_frequency(f))
+    sleep(0.2)
+    tb.stop()
+
+def sb4():
+    print("Goodbye! 26 unten")
+    global tb
+    f = 523.25
+    tb.play(Tone.from_frequency(f))
+    sleep(0.2)
+    tb.stop()
+
+def sb5():
+    print("Goodbye! 6 a")
+    global tb
+    f = 587.33
+    tb.play(Tone.from_frequency(f))
+    sleep(0.2)
+    tb.stop()
+
+def sb6():
+    print("Goodbye! 12 b")
+    global tb
+    f = 659.25
+    tb.play(Tone.from_frequency(f))
+    sleep(5.2)
+    tb.stop()
+
+
+button1 = Button(16)
+button2 = Button(19)
+button3 = Button(20)
+button4 = Button(26)
+button5 = Button(6)
+button6 = Button(12)
+
+button1.when_pressed = sb1
+
+button2.when_pressed = sb2
+button3.when_pressed = sb3
+button4.when_pressed = sb4
+button5.when_pressed = sb5
+button6.when_pressed = sb6
+pause()
--- mainmenu.py
+++ mainmenu.py
@@ -2,28 +2,27 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
+from gpiozero import Button
+import logging
+from waveshare_epd import epd2in13_V4
+import time
+from PIL import Image, ImageDraw, ImageFont
+from signal import pause
+
 picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
 
-from gpiozero import Button
-import logging
-from waveshare_epd import epd2in13_V4
-import time
-from PIL import Image, ImageDraw
-from signal import pause
-
-
 def obenlinks():
     global draw1, final_image
     print("o")
-    
-    draw1.rectangle([(248, 120), (127, 63)], outline = 0, width=3)
-    draw1.rectangle([(123, 120), (2, 59)], outline = 255, width=3)
-    draw1.rectangle([(248, 59), (123, 2)], outline = 255, width=3)
-    draw1.rectangle([(123, 59), (2, 2)], outline = 255, width=3)
+
+    draw1.rectangle([(231, 89), (134, 57)], outline=0, width=3)
+    draw1.rectangle([(115, 89), (19, 57)], outline=255, width=3)
+    draw1.rectangle([(231, 40), (134, 8)], outline=255, width=3)
+    draw1.rectangle([(115, 40), (19, 8)], outline=255, width=3)
     time.sleep(0.5)
     epd.displayPartial(epd.getbuffer(final_image))
 
@@ -31,10 +30,10 @@ def obenlinks():
 def obenrechts():
     global draw1, final_image
     print("r")
-    draw1.rectangle([(248, 120), (127, 63)], outline = 255, width=3)
-    draw1.rectangle([(123, 120), (2, 59)], outline = 0, width=3)
-    draw1.rectangle([(248, 59), (123, 2)], outline = 255, width=3)
-    draw1.rectangle([(123, 59), (2, 2)], outline = 255, width=3)
+    draw1.rectangle([(231, 89), (134, 57)], outline=255, width=3)
+    draw1.rectangle([(115, 89), (19, 57)], outline=0, width=3)
+    draw1.rectangle([(231, 40), (134, 8)], outline=255, width=3)
+    draw1.rectangle([(115, 40), (19, 8)], outline=255, width=3)
     time.sleep(0.5)
     epd.displayPartial(epd.getbuffer(final_image))
 
@@ -42,10 +41,10 @@ def obenrechts():
 def untenlinks():
     global draw1, final_image
     print("u")
-    draw1.rectangle([(248, 120), (127, 63)], outline = 255, width=3)
-    draw1.rectangle([(123, 120), (2, 59)], outline = 255, width=3)
-    draw1.rectangle([(248, 59), (127, 2)], outline = 0, width=3)
-    draw1.rectangle([(123, 59), (2, 2)], outline = 255, width=3)
+    draw1.rectangle([(231, 89), (134, 57)], outline=255, width=3)
+    draw1.rectangle([(115, 89), (19, 57)], outline=255, width=3)
+    draw1.rectangle([(231, 40), (134, 8)], outline=0, width=3)
+    draw1.rectangle([(115, 40), (19, 8)], outline=255, width=3)
     time.sleep(0.5)
     epd.displayPartial(epd.getbuffer(final_image))
 
@@ -53,15 +52,15 @@ def untenlinks():
 def untenrechts():
     global draw1, final_image
     print("l")
-    draw1.rectangle([(248, 120), (127, 63)], outline = 255, width=3)
-    draw1.rectangle([(123, 120), (2, 59)], outline = 255, width=3)
-    draw1.rectangle([(248, 59), (127, 2)], outline = 255, width=3)
-    draw1.rectangle([(123, 59), (2, 2)], outline = 0, width=3)
+    draw1.rectangle([(231, 89), (134, 57)], outline=255, width=3)
+    draw1.rectangle([(115, 89), (19, 57)], outline=255, width=3)
+    draw1.rectangle([(231, 40), (134, 8)], outline=255, width=3)
+    draw1.rectangle([(115, 40), (19, 8)], outline=0, width=3)
     time.sleep(0.5)
     epd.displayPartial(epd.getbuffer(final_image))
 
 
-#gpio button
+# gpio button
 btn_links = Button(19)
 btn_rechts = Button(20)
 btn_oben = Button(16)
@@ -70,10 +69,11 @@ btn_a = Button(6)
 btn_b = Button(12)
 
 btn_links.when_pressed = untenlinks
-btn_rechts.when_pressed = untenrechts
-btn_oben.when_pressed = obenrechts
-btn_unten.when_pressed = obenlinks
+btn_rechts.when_pressed = obenrechts
+btn_oben.when_pressed = obenlinks
+btn_unten.when_pressed = untenrechts
 
+font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
 
 try:
     epd = epd2in13_V4.EPD()
@@ -81,7 +81,7 @@ try:
     epd.Clear(0xFF)
 
     image = Image.new('1', (epd.height, epd.width), 255)
-    
+
     draw = ImageDraw.Draw(image)
 
     bmp = Image.open(os.path.join(picdir, 'cat1.bmp'))
@@ -89,10 +89,12 @@ try:
     bmp2 = Image.open(os.path.join(picdir, 'cat3.bmp'))
     bmp3 = Image.open(os.path.join(picdir, 'cat4.bmp'))
 
-    image.paste(bmp, (20, 5))
-    image.paste(bmp1, (140, 5))
-    image.paste(bmp2, (20, 64))
-    image.paste(bmp3, (140, 64))
+    draw.text((115, 8), 'Pika', font = font15, fill = 0)
+
+    image.paste(bmp, (19, 27))
+    image.paste(bmp1, (135, 27))
+    image.paste(bmp2, (19, 75))
+    image.paste(bmp3, (135, 75))
 
     rotated_image = image.rotate(180, expand=True)  # rotate
 
@@ -106,13 +108,11 @@ try:
 
     final_image = Image.new('1', (rotated_width, rotated_height), 255)
 
-    #final_image = final_image.rotate(180)
+    # final_image = final_image.rotate(180)
     final_image.paste(rotated_image)
     draw1 = ImageDraw.Draw(final_image)
-    
 
     epd.displayPartial(epd.getbuffer(final_image))
-
 
     num = 0
     while (True):

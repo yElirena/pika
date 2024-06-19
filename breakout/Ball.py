0e1cae9cbf1d1f24e7fb72f51cd4af5a539ecc8f
yElirena <153385040+yElirena@users.noreply.github.com>
2024-06-19 14:10:21 +0200

    ball bounce not working

--- breakout/Ball.py
+++ breakout/Ball.py
@@ -1,11 +1,14 @@
 from PIL import Image, ImageDraw
 from waveshare_epd import epd2in13_V4
 
+movement = 10
 
 class Ball:
     def __init__(self, epd):
         self.epd = epd
         self.size = [(40, 45), (45, 50)]
+        self.moveX = movement
+        self.moveY = movement
     
     def initiateBall(self, draw):
         draw.ellipse(self.size, fill=0)
@@ -16,7 +19,26 @@ class Ball:
     def move(self, draw):
         self.clearBall(draw)
 
-        newSize = [(self.size[0][0] - 10, self.size[0][1] - 10),
-                    (self.size[1][0] - 10, self.size[1][1] - 10)]
+        newSize = [(self.size[0][0] + self.moveX, self.size[0][1] + self.moveY),
+                    (self.size[1][0] + self.moveX, self.size[1][1] + self.moveY)]
         self.size = newSize
         self.initiateBall(draw)
+    
+    def checkCollisionWithWalls(self):
+        #hopefully detects collision left and right walls
+        if self.size[0][0] < -self.epd.width or self.size[0][0] > self.epd.width:
+            self.moveX *= -1
+            return
+        #hopefully detects collision upper wall
+        if self.size[0][1] > self.epd.height:
+            self.moveY *= -1
+            return
+        #hopefully detects collosion with bottom wall
+        if self.size[0][1] < -self.epd.height:
+            self.reset()
+            return
+    
+    def reset(self):
+        self.size = [(40, 45), (45, 50)]
+        self.moveX = movement
+        self.moveY = movement
